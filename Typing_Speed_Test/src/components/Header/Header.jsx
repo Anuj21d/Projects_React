60b41d1d2d8c1351fd0b458ad20115c4f1b39547
Anuj21d <anujwork2410@gmail.com>
2026-06-18 22:29:21 +0530

    update image path

--- Typing_Speed_Test/src/components/Header/Header.jsx
+++ Typing_Speed_Test/src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
 import React, { useContext } from "react";
 import { TestContext } from "/src/context/TestContext/TestContext.jsx";
+import Logo from '../../assets/images/logo-large.svg'
+import Trophy from '../../assets/images/icon-personal-best.svg'
 
 const Header = () => {
   const { bestWpm } = useContext(TestContext);
   return (
     <div className="flex justify-between items-center py-8">
-      <img src="/src/assets/images/logo-large.svg" alt="Logo Svg" />
+      <img src={Logo} alt="Logo Svg" />
       <div className="flex items-center gap-2 text-neutral-400 text-2xl font-medium">
         <img
-          src="/src/assets/images/icon-personal-best.svg"
+          src={Trophy}
           alt="Trophy Logo"
         />
         <h2>
